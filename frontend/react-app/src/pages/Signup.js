47e4ab7e4e60c667284eb8218622edf300c38660
Ankitha R B <ankithabelagundi@gmail.com>
2026-03-15 16:25:46 +0530

    new update

--- frontend/react-app/src/pages/Signup.js
+++ frontend/react-app/src/pages/Signup.js
@@ -1,6 +1,11 @@
 import { useState,useContext } from "react";
 import API from "../api";
 import { AuthContext } from "../context/AuthContext";
-import { useNavigate 
-    
-} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+export default function Signup(){
+    const[name,setName]=useState("")
+    const[email,setEmail]=useState("")
+    const [password,setPassword]=useState("")
+    const {login}= 
+}
